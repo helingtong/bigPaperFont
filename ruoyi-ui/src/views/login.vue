0bd6ed9a34f6755f08706adac98193f69c7b7a2f
xiaohe <3128324742@qq.com>
2023-12-21 21:32:48 +0800

    第一次提交前端 若依框架样式

--- ruoyi-ui/src/views/login.vue
+++ ruoyi-ui/src/views/login.vue
@@ -1,7 +1,8 @@
 <template>
+<!--注册界面-->
   <div class="login">
     <el-form ref="loginForm" :model="loginForm" :rules="loginRules" class="login-form">
-      <h3 class="title">若依后台管理系统</h3>
+      <h3 class="title">语义约束服务平台</h3>
       <el-form-item prop="username">
         <el-input
           v-model="loginForm.username"
@@ -56,7 +57,7 @@
     </el-form>
     <!--  底部  -->
     <div class="el-login-footer">
-      <span>Copyright © 2018-2023 ruoyi.vip All Rights Reserved.</span>
+      <span>Copyright © 2018-2023 constraint All Rights Reserved.</span>
     </div>
   </div>
 </template>
